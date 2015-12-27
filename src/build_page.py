57814e3fee6a2d7132b5e0bad1aabc4eb60b0306
Colin Wallace <wallacoloo@gmail.com>
2015-12-26 21:47:12 -0800

    Basic makefile w/ build & clean

---
+++ src/build_page.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+from jinja2 import Environment, PackageLoader
+
+def render_page(in_path):
+    env = Environment(loader=PackageLoader("__main__", 'templates'))
+    template = env.get_template(in_path)
+    return template.render()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: %s <input html template> <output path>" %(sys.argv[0] if len(sys.argv) else "build_page.py"))
+        sys.exit(1)
+
+    in_path, out_path = sys.argv[1:]
+    # Remove the assumed 'templates/' path prefix.
+    if in_path.startswith("templates/"):
+        in_path = in_path[len("templates/"):]
+    data = render_page(in_path)
+    f = open(out_path, "w")
+    f.write(data)
