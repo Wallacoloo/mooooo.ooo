6b15dce1027feb65ac84fd23d4321c882faa4a7b
Colin Wallace <wallacoloo@gmail.com>
2016-01-02 23:14:25 -0800

    Add publish date extraction to the blog entries; split CSS into multiple files, but compile it into one file before publishing

--- src/build_page.py
+++ src/build_page.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+# Requires the following pip packages:
+# python-dateutil, jinja2
 
 import json, os, subprocess, sys
 
+import dateutil.parser
 from jinja2 import Environment, PackageLoader
 
 
@@ -29,21 +32,43 @@ def filter_into_tag(value):
 
     return value
 
-def get_authors_of_file(filename):
-    """Return a set of Author objects that have commits associated with the
-    file"""
+def filter_friendly_date(date):
+    """Takes a datetime.datetime object, returns a friendly version of the date, e.g. 'Jan 22, 2015'"""
+    return date.strftime("%b %d, %Y")
+
+def get_git_log(filename):
     proc = subprocess.Popen(["git", "log", "--", filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
     assert stderr == b''
+    return str(stdout).replace("\\n", "\n")
+
+def get_authors_of_file(filename):
+    """Return a set of Author objects that have commits associated with the
+    file"""
+    git_log = get_git_log(filename)
 
     authors = set()
-    for line in str(stdout).replace("\\n", "\n").split("\n"):
+    for line in git_log.split("\n"):
         if line.startswith("Author:"):
-            line = line.replace("Author: ", "")
+            line = line[len("Author:"):]
             name, _email_with_right_angle_bracket = line.split("<")
             name = name.strip()
             authors.add(Author(name))
     return list(authors)
+
+def get_edit_dates_of_file(filename):
+    """Returns an ordered list of the dates at which a file was modified,
+    according to the git metadata"""
+    git_log = get_git_log(filename)
+
+    edit_dates = []
+    for line in git_log.split("\n"):
+        if line.startswith("Date:"):
+            line = line[len("Date:")]
+            date = dateutil.parser.parse(line.strip())
+            edit_dates.append(date)
+    edit_dates.sort()
+    return edit_dates
 
 class Page(object):
     def __init__(self, path):
@@ -68,6 +93,14 @@ class BlogEntry(Page):
     @property
     def template_path_on_disk(self):
         return self._template_path_on_disk
+
+    @property
+    def pub_date(self):
+        return get_edit_dates_of_file(self._template_path_on_disk)[0]
+
+    @property
+    def last_edit_date(self):
+        return get_edit_dates_of_file(self._template_path_on_disk)[-1]
 
 class Author(object):
     def __init__(self, name):
@@ -98,8 +131,13 @@ def render_page(config, in_path, out_path):
     env.globals.update(config)
     blog_entries = get_blog_objects(env)
     env.globals["blog_entries"] = blog_entries
-    env.globals["pages"] = { "index": Page("index.html"), "about": Page("pages/about/index.html")}
+    env.globals["pages"] = {
+        "index": Page("index.html"),
+        "about": Page("pages/about/index.html"),
+        "global_css": Page("res/css/global.css"),
+    }
     env.filters["into_tag"] = filter_into_tag
+    env.filters["friendly_date"] = filter_friendly_date
     env.filters["to_rel_path"] = to_rel_path
 
     for entry in blog_entries:
