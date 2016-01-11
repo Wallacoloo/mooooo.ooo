9b9c202280835f6083b974c6ea5f93c3addb6778
Colin Wallace <wallacoloo@gmail.com>
2016-01-10 20:09:17 -0800

    Formatting fix + add link to comment on the article

--- src/build_page.py
+++ src/build_page.py
@@ -4,9 +4,8 @@
 
 import json, os, subprocess, sys
 
-import dateutil.parser
+import dateutil.parser, jinja2, PIL.Image
 from jinja2 import Environment, PackageLoader
-import PIL.Image;
 
 
 # Directory in which blog entries are stored
@@ -62,6 +61,25 @@ def filter_detailed_date(date):
     """Takes a datetime.datetime object, returns a detaile version of the date, e.g. 'Jan 22, 2015 at 8:09 PM PST'"""
     return date.strftime("%b %d, %Y at %I:%M %p PST")
 
+def filter_drop_null_values(dict):
+    """Returns a new dict that contains only the items for which bool(item.value) == True
+    Example:
+    >>> filter_drop_null_values(dict(x=2, y=None, z="", k="yes", j="False"))
+    dict(x=2, k="yes", j="False")
+    """
+
+    filt = {}
+    for k, v in dict.items():
+        if v:
+            filt[k] = v
+    return filt
+
+def filter_url_with_args(path, **kwargs):
+    """Returns `path` + "?" + urlencode(kwargs),
+    or just `path` if there are no kwargs
+    """
+    urlargs = "?" + jinja2.filters.do_urlencode(kwargs) if kwargs else ""
+    return path + urlargs
 
 def get_unparsed_commits(filename):
     """Returns an array of commit dicts, where each entry looks like:
@@ -136,6 +154,17 @@ class Page(object):
         """Path relative to the build directory (i.e. the URL of the resource, minus the domain name"""
         assert self._path is not None
         return self._path
+    @property
+    def friendly_path(self):
+        """Returns the (unique) identifier/path for this page, but in the most friendly manner, i.e. no trailing 'index.html' or slash"""
+        p = self.path
+        if p.endswith("index.html"):
+            p = p[:-len("index.html")]
+        if p.endswith("/"):
+            p = p[:-1]
+        if p == "":
+            p = "/"
+        return p
     @property
     def repo_page(self):
         """Path where the user can view the source/revision history of a file & submit pull requests."""
@@ -214,6 +243,8 @@ class Page(object):
         env.filters["into_tag"] = filter_into_tag
         env.filters["friendly_date"] = filter_friendly_date
         env.filters["detailed_date"] = filter_detailed_date
+        env.filters["drop_null_values"] = filter_drop_null_values
+        env.filters["url_with_args"] = filter_url_with_args
         env.filters["to_rel_path"] = to_rel_path
         env.globals["page"] = self
         # Expose these types for passing to the `set_page_type` macro
@@ -246,7 +277,15 @@ class Css(Page):
     pass
 
 class BlogEntry(Page):
-    pass 
+    @property
+    def comment_email(self):
+        """The email address that should be used to send comments to this blog entry"""
+        return config["social"]["comment_email"]
+
+    @property
+    def comment_email_subject(self):
+        """The subject tagline that should be used to identify a comment to this article"""
+        return "[%s]" %self.friendly_path
 
 
 class HomePage(Page):
