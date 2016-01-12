9d4642b2b4f0799f4eebbe3e0d1a6a5963cbe9d9
Colin Wallace <wallacoloo@gmail.com>
2016-01-12 14:28:50 -0800

    Outline for make.py

--- src/make.py
+++ src/make.py
@@ -11,11 +11,31 @@ def make(page=None):
     This will result in an up-to-date website in the build directory,
     but it will not be implicitly published.
     If page is an instance of page_info.Page, build just that page"""
-    print("[FIXME] make.make() not implemented")
+    if page is None:
+        import page_info
+        for page in page_info.get_pages():
+            make(page)
+    else:
+        print("[make %r]" %page)
+        print("[FIXME] make.make() not implemented")
+
+def make_deps(page=None):
+    """Determine the dependencies for building the given page,
+    and create the proper Makefile rules for that,
+    inserting them into <page>.deps.
+    If page is None, this will create ALL dependencies,
+    otherwise, page should be a page_info.Page object."""
+    print("[FIXME] make.make_deps() not implemented")
 
 def publish():
     """Publish the website to IPFS and update any necessary host records"""
-    print("[FIXME] make.publish() not implemented")
+    if page is None:
+        import page_info
+        for page in page_info.get_pages():
+            make_deps(page)
+    else:
+        print("[make_deps %r]" %page)
+        print("[FIXME] make.publish() not implemented")
 
 
 if __name__ == "__main__":
@@ -25,10 +45,11 @@ if __name__ == "__main__":
     targets = sys.argv[1:]
 
     for target in targets:
-        if target == "all":
-            make()
-        elif target == "publish":
-            publish()
+        if target == "dependencies.includes":
+            make_deps()
+        elif target.endswith(".deps"):
+            page = page_info.get_pages()[target[:-len(".deps")]]
+            make_deps(page)
         else:
             page = page_info.get_pages()[target]
             make(page)
