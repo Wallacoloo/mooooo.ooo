fb0550fc3423cdea6bc9c9c52943802adcd029d2
Colin Wallace <wallacoloo@gmail.com>
2016-03-10 18:16:26 -0800

    Use jpgcrush to losslessly compress the jpegs

--- get-deps-arch.sh
+++ get-deps-arch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Installs the pre-requisites for Arch Linux
-pacman -Su --needed --noconfirm fontforge go-ipfs jpegoptim optipng python python-dateutil python-jinja python-pillow python-requests
+pacman -Su --needed --noconfirm fontforge go-ipfs optipng python python-dateutil python-jinja python-pillow python-requests
 
 alias makepkg="sudo -u nobody makepkg"
 
@@ -8,7 +8,12 @@ cd /tmp
 rm -rf webify/ && sudo -u nobody git clone https://aur.archlinux.org/webify.git && cd webify/
 makepkg
 pacman --noconfirm -U *.pkg.tar.xz 
+
 rm -rf woff2-git && sudo -u nobody git clone https://aur.archlinux.org/woff2-git.git && cd woff2-git
+makepkg
+pacman --noconfirm -U *.pkg.tar.xz 
+
+rm -rf jpgcrush/ && sudo -u nobody git clone https://aur.archlinux.org/jpgcrush.git && cd jpgcrush/
 makepkg
 pacman --noconfirm -U *.pkg.tar.xz 
 rm -rf $(pwd)
