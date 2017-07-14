895d39dbb6cb5bb3b98581eba596b9b3426b7ce7
Colin Wallace <colin.wallace@flukenetworks.com>
2017-07-14 11:30:55 -0700

    Add missing deps

--- get-deps-arch.sh
+++ get-deps-arch.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # Installs the pre-requisites for Arch Linux
-pacman -Su --needed --noconfirm fontforge go-ipfs optipng python python-dateutil python-jinja python-pillow python-requests
+sudo pacman -Su --needed --noconfirm ffmpeg fontforge go-ipfs optipng python python-dateutil python-jinja python-pillow python-requests scour texlive-core
 pacaur -S jpgcrush python-joblib webify woff2-git
+
+# TODO: drop npm dependency by trying a different tex2svg script
+sudo pacman -S npm
+sudo npm install -g tex-equation-to-svg
